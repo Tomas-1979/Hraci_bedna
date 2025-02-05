fe675d5ccfd4cd9704d2854f6bed68832bc778c5
Tomas-1979 <piticko1@gmail.com>
2025-02-05 19:36:45 +0100

    repaired doubleclick

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+let reportBody = document.querySelector("#ReportBody");
 let gameBox = document.querySelector("#gameBox");
 let boxes = document.querySelector("#Boxes");
 let firstColor = document.querySelector("#firstColor");
@@ -13,6 +14,9 @@ let spinBtn = document.querySelector("#spinBtn");
 let btnArrLeftPos = document.querySelector("#leftArrToRight");
 let btnArrRightPos = document.querySelector("#rightArrToLeft");
 let resetBtn = document.querySelector("#resetBtn");
+
+// tlačítko - ošetčení dvojkliku
+let clickValue = true;
 
 // pole barev
 const colors = ["red", "pink", "blue", "yellow", "purple", "orange"];
@@ -46,91 +50,105 @@ resetBtn.addEventListener("click", () => {
 //
 // kliknutí na tlačítko pro změnu barev
 //
+
 spinBtn.addEventListener("click", () => {
-  // animace klikacího tlačítka
-  animateButton();
+  if (clickValue) {
+    clickValue = false;
 
-  // rotace barev ve čtvercích
-  firstColor.style.transform = rotate();
-  secondColor.style.transform = rotate();
-  thirdColor.style.transform = rotate();
+    // animace klikacího tlačítka
+    animateButton();
 
-  // funkce pro generování barev
-  const getRandomColor = () => {
-    return colors[Math.floor(Math.random() * colors.length)];
-  };
-  let randColor1 = getRandomColor();
-  let randColor2 = getRandomColor();
-  let randColor3 = getRandomColor();
+    // rotace barev ve čtvercích
+    firstColor.style.transform = rotate();
+    secondColor.style.transform = rotate();
+    thirdColor.style.transform = rotate();
 
-  // přiřazení barev
-  firstColor.style.backgroundColor = randColor1;
-  secondColor.style.backgroundColor = randColor2;
-  thirdColor.style.backgroundColor = randColor3;
+    // funkce pro generování barev
+    const getRandomColor = () => {
+      return colors[Math.floor(Math.random() * colors.length)];
+    };
+    let randColor1 = getRandomColor();
+    let randColor2 = getRandomColor();
+    let randColor3 = getRandomColor();
 
-  //
-  // logika hry
-  //
-  // podmínky pro přičítání bodů
-  if (
-    randColor1 === randColor2 &&
-    randColor2 === randColor3 &&
-    randColor3 === randColor1
-  ) {
-    numClick.textContent++;
-    score.textContent = parseInt(score.textContent) + 500;
-  } else if (
-    randColor1 === randColor2 ||
-    randColor2 === randColor3 ||
-    randColor3 === randColor1
-  ) {
-    numClick.textContent++;
-    score.textContent = parseInt(score.textContent) + 200;
-  } else {
-    numClick.textContent++;
-  }
-  //
-  // podmínky pro zobrazení výherního textu
-  //
-  if (
-    // prohra
-    parseInt(numClick.textContent) === 5 &&
-    parseInt(score.textContent) < 600
-  ) {
-    setTimeout(() => {
+    // přiřazení barev
+    firstColor.style.backgroundColor = randColor1;
+    secondColor.style.backgroundColor = randColor2;
+    thirdColor.style.backgroundColor = randColor3;
+
+    //
+    // logika hry
+    //
+    // podmínky pro přičítání bodů
+    if (
+      randColor1 === randColor2 &&
+      randColor2 === randColor3 &&
+      randColor3 === randColor1
+    ) {
+      numClick.textContent++;
+      score.textContent = parseInt(score.textContent) + 500;
+    } else if (
+      randColor1 === randColor2 ||
+      randColor2 === randColor3 ||
+      randColor3 === randColor1
+    ) {
+      numClick.textContent++;
+      score.textContent = parseInt(score.textContent) + 200;
+    } else {
+      numClick.textContent++;
+    }
+    //
+    // podmínky pro zobrazení výherního textu
+    //
+    if (
+      // prohra
+      parseInt(numClick.textContent) === 5 &&
+      parseInt(score.textContent) < 600
+    ) {
       spinBtn.classList.add("invisible");
-    }, 200);
-    setTimeout(() => {
-      textWinLose.textContent = "Prohra! :(";
-      textWinLose.style.transition = "transform 0.9s";
-      textWinLose.style.transform = "scale(1.3)";
       setTimeout(() => {
-        textWinLose.style.transform = "scale(0)";
-      }, 800);
-    }, 500);
-    setTimeout(() => {
-      boxes.classList.add("invisible");
-      resetBtn.classList.remove("invisible");
-    }, 2000);
-  } else if (
-    // výhra
-    parseInt(numClick.textContent) <= 5 &&
-    parseInt(score.textContent) >= 600
-  ) {
-    setTimeout(() => {
-      spinBtn.classList.add("invisible");
-    }, 200);
-    setTimeout(() => {
-      textWinLose.textContent = "Výhra! :)";
-      textWinLose.style.transition = "transform 0.9s";
-      textWinLose.style.transform = "scale(.3)";
-      setTimeout(() => {
+        textWinLose.textContent = "Prohra! :(";
+        textWinLose.style.transition = "transform 0.9s";
         textWinLose.style.transform = "scale(1.3)";
-      }, 800);
-    }, 500);
+        setTimeout(() => {
+          textWinLose.style.transform = "scale(0)";
+        }, 1000);
+      }, 500);
+      setTimeout(() => {
+        boxes.classList.add("invisible");
+        resetBtn.classList.remove("invisible");
+      }, 2000);
+    } else if (
+      // výhra
+      parseInt(numClick.textContent) <= 5 &&
+      parseInt(score.textContent) >= 600
+    ) {
+      spinBtn.classList.add("invisible");
+      setTimeout(() => {
+        textWinLose.textContent = "Výhra! :)";
+        textWinLose.style.transition = "transform 0.9s";
+        textWinLose.style.transform = "scale(.3)";
+        setTimeout(() => {
+          textWinLose.style.transform = "scale(1.3)";
+        }, 1000);
+      }, 500);
+      setTimeout(() => {
+        boxes.classList.add("invisible");
+        resetBtn.classList.remove("invisible");
+      }, 2000);
+    }
+
+    // ošetření dvojkliku
     setTimeout(() => {
-      boxes.classList.add("invisible");
-      resetBtn.classList.remove("invisible");
-    }, 2000);
+      clickValue = true;
+    }, 200);
+  } else {
+    spinBtn.style.display = "none";
+    reportBody.style.display = "block";
+    reportBody.textContent = `krapet zvolni...`;
+    setTimeout(() => {
+      spinBtn.style.display = "block";
+      reportBody.style.display = "none";
+    }, 3000);
   }
 });
